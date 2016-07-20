c94d41f9d0f4be35426c6004e2976a682a60a5cc
Joshua Hamilton <joshua@tinydog.io>
2016-07-20 14:18:37 -0400

    Move env var

--- vagrantfiles/ubuntu-16.04/php-maria/provision.sh
+++ vagrantfiles/ubuntu-16.04/php-maria/provision.sh
@@ -28,7 +28,8 @@ sudo ufw allow 9000
 sudo systemctl enable apache2
 sudo a2enmod rewrite
 
-sudo export DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -y mysql-server
 sudo systemctl enable mysqld
 
 sudo apt-get install -y php libapache2-mod-php php-mcrypt php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc
