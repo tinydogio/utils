bf6657f6a04d375fe7c1c708d2a6429e9f4174b7
Joshua Hamilton <joshua@tinydog.io>
2016-12-22 14:49:46 -0500

    Update letsencrypt renewal routine.

--- server-config/ubuntu-16.04/node-mongo/letsencrypt.sh
+++ server-config/ubuntu-16.04/node-mongo/letsencrypt.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 sudo apt-get install -y letsencrypt
+sudo crontab -l | { cat; echo "28 2 * * 1 /bin/systemctl stop nginx"; } | crontab -
 sudo crontab -l | { cat; echo "30 2 * * 1 /usr/bin/letsencrypt renew >> /var/log/le-renew.log"; } | crontab -
-sudo crontab -l | { cat; echo "35 2 * * 1 /bin/systemctl reload nginx"; } | crontab -
+sudo crontab -l | { cat; echo "35 2 * * 1 /bin/systemctl start nginx"; } | crontab -
