b784350242b453e96d84f6ca980717ad86b64672
Joshua Hamilton <joshua@ungana.com>
2018-04-11 20:05:03 -0400

    Update letsencrypt routine to use certbot.

--- server-config/ubuntu-16.04/node-mongo/letsencrypt.sh
+++ server-config/ubuntu-16.04/node-mongo/letsencrypt.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get install -y letsencrypt
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:certbot/certbot
+sudo apt-get update
+sudo apt-get install certbot
 sudo crontab -l | { cat; echo "28 2 * * 1 /bin/systemctl stop nginx"; } | crontab -
-sudo crontab -l | { cat; echo "30 2 * * 1 /usr/bin/letsencrypt renew >> /var/log/le-renew.log"; } | crontab -
+sudo crontab -l | { cat; echo "30 2 * * 1 /usr/bin/certbot renew >> /var/log/certbot-renew.log"; } | crontab -
 sudo crontab -l | { cat; echo "35 2 * * 1 /bin/systemctl start nginx"; } | crontab -
