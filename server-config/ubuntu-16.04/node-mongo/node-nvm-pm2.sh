cc006e52217a09a5796d751253bb44cd419e03ec
Joshua Hamilton <joshua@ungana.com>
2018-04-11 20:37:20 -0400

    Update routine to make sure node and pm2 install.

--- server-config/ubuntu-16.04/node-mongo/node-nvm-pm2.sh
+++ server-config/ubuntu-16.04/node-mongo/node-nvm-pm2.sh
@@ -3,10 +3,14 @@
 sudo curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 sudo echo -e "\n\n# NVM Source\nsource ~/.nvm/nvm.sh" >> ~/.bashrc
 sudo source ~/.nvm/nvm.sh
+
+sudo exec bash
 sudo nvm install v8.11.1
 sudo nvm alias default v8.11.1
 
 sudo npm install pm2@latest -g
+
+sudo exec bash
 sudo pm2 completion install
 sudo pm2 save
 sudo pm2 startup ubuntu
